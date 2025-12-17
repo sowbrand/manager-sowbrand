7b1318addc4e66d604b388ff61853ebd7aec26b6
sowbrand <sowbrandbrasil@gmail.com>
2025-12-17 09:59:53 -0300

    versao-final-dados-empresa

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 // Configuração padrão completa para evitar erros de tipo
 export const DEFAULT_COMPANY_SETTINGS = {
   company_name: 'Sow Brand',
-  cnpj: '',           // Campo novo obrigatório
-  contact_email: '',
-  phone: '',          // Campo novo obrigatório
-  address: '',
+  cnpj: '26.224.938/0001-89',
+  contact_email: 'contato@sowbrandbrasil.com.br',
+  phone: '(47) 99197-6742',
+  address: 'Rua Fermino Görll, 115, Reta, São Francisco do Sul - SC',
   footer_text: 'Todos os direitos reservados.',
   logo_url: ''
 };
